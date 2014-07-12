a59908314f418cf28aca9bee2f1813601a71596c
Mimee <xxu@hmc.edu>
2014-07-11 23:10:43 -0700

    Initial Commit

---
+++ Allergy/LHAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  LHAppDelegate.h
+//  Allergy
+//
+//  Created by Xinlei Xu on 7/11/14.
+//  Copyright (c) 2014 LinkedIn Hackday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Allergy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Allergy
+//
+//  Created by Xinlei Xu on 7/11/14.
+//  Copyright (c) 2014 LinkedIn Hackday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LHAppDelegate class]));
+    }
+}
